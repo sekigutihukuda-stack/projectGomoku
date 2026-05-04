03ae789e26e5380c755eb4e9d5389034060817b4
TaketoFukuda <sekiguti.hukuda@gmail.com>
2026-05-04 16:20:18 +0900

    ボタンを押したら配列が編集されるようにした

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 
-function Square() {
-  return <button className = "square"></button>  
+function Square({onSquareClick}) {
+  return <button className = "square" onClick = {onSquareClick}></button>  
 }
 
 export default function Board() {
@@ -28,39 +28,39 @@ export default function Board() {
     <>
       <div className = "board">
         <div className = "board-row">
-          <Square />
-          <Square />
-          <Square />
-          <Square />
-          <Square />
+          <Square onSquareClick={() => handleClick(0)}/>
+          <Square onSquareClick={() => handleClick(1)}/>
+          <Square onSquareClick={() => handleClick(2)}/>
+          <Square onSquareClick={() => handleClick(3)}/>
+          <Square onSquareClick={() => handleClick(4)}/>
         </div>
         <div className = "board-row">
-          <Square />
-          <Square />
-          <Square />
-          <Square />
-          <Square />
+          <Square onSquareClick={() => handleClick(5)}/>
+          <Square onSquareClick={() => handleClick(6)}/>
+          <Square onSquareClick={() => handleClick(7)}/>
+          <Square onSquareClick={() => handleClick(8)}/>
+          <Square onSquareClick={() => handleClick(9)}/>
         </div>
         <div className = "board-row">
-          <Square />
-          <Square />
-          <Square />
-          <Square />
-          <Square />
+          <Square onSquareClick={() => handleClick(10)}/>
+          <Square onSquareClick={() => handleClick(11)}/>
+          <Square onSquareClick={() => handleClick(12)}/>
+          <Square onSquareClick={() => handleClick(13)}/>
+          <Square onSquareClick={() => handleClick(14)}/>
         </div>
         <div className = "board-row">
-          <Square />
-          <Square />
-          <Square />
-          <Square />
-          <Square />
+          <Square onSquareClick={() => handleClick(15)}/>
+          <Square onSquareClick={() => handleClick(16)}/>
+          <Square onSquareClick={() => handleClick(17)}/>
+          <Square onSquareClick={() => handleClick(18)}/>
+          <Square onSquareClick={() => handleClick(19)}/>
         </div>
         <div className = "board-row">
-          <Square />
-          <Square />
-          <Square />
-          <Square />
-          <Square />
+          <Square onSquareClick={() => handleClick(20)}/>
+          <Square onSquareClick={() => handleClick(21)}/>
+          <Square onSquareClick={() => handleClick(22)}/>
+          <Square onSquareClick={() => handleClick(23)}/>
+          <Square onSquareClick={() => handleClick(24)}/>
         </div>
       </div>
     </>
