d2431b280f867f8033a2b9ba22f86f41bfb97d8a
TaketoFukuda <sekiguti.hukuda@gmail.com>
2026-05-04 16:51:33 +0900

    handleClick()関数内のnextSquaresの定義に関する修正

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,11 @@ export default function Board() {
   const [squares,setSquares] = useState(Array.from({ length:5 }, ()=>Array.from({ length:5}, ()=>[])));
 
   function handleClick(i,j) {
-    const nextSquares = squares.map((list) => {return [...list];});
+    const nextSquares = squares.map((row) => {
+      return row.map((cell) => {
+        return [...cell];}
+      );}
+    );
 
     if (xIsNext) {
       nextSquares[i][j].push("X");
@@ -49,9 +53,9 @@ export default function Board() {
           <Square onSquareClick={() => handleClick(2,4)}/>
         </div>
         <div className = "board-row">
+          <Square onSquareClick={() => handleClick(3,0)}/>
           <Square onSquareClick={() => handleClick(3,1)}/>
           <Square onSquareClick={() => handleClick(3,2)}/>
-          <Square onSquareClick={() => handleClick(3,3)}/>
           <Square onSquareClick={() => handleClick(3,3)}/>
           <Square onSquareClick={() => handleClick(3,4)}/>
         </div>
