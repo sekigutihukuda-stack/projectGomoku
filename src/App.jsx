89c66ff79648c3f11c7b5b7a5e3b7bfd7282b16a
TaketoFukuda <sekiguti.hukuda@gmail.com>
2026-05-04 16:04:17 +0900

    五目並べの進行状況を保存する配列及びその周辺のプログラムの作成

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,21 @@ function Square() {
 }
 
 export default function Board() {
+  const [xIsNext,setXIsNext] = useState(true);
+  const [squares,setSquares] = useState(Array.from({ length:25 }, ()=>[]));
+
+  function handleClick(i) {
+    const nextSquares = squares.map((list) => {return [...list];});
+
+    if (xIsNext) {
+      nextSquares[i].push("X");
+    } else {
+      nextSquares[i].push("O");
+    }
+    setSquares(nextSquares);
+    setXIsNext(!xIsNext);
+  }
+
   return(
     <>
       <div className = "board">
