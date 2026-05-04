42d11030ce628448e822c30c490f818691b93b8c
TaketoFukuda <sekiguti.hukuda@gmail.com>
2026-05-04 14:22:23 +0900

    ボタンの追加及びサイズの調整

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,48 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 
-function App() {
-  
+function Square() {
+  return <button className = "square"></button>  
 }
 
-export default App
+export default function Board() {
+  return(
+    <>
+      <div className = "board-row">
+        <Square />
+        <Square />
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className = "board-row">
+        <Square />
+        <Square />
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className = "board-row">
+        <Square />
+        <Square />
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className = "board-row">
+        <Square />
+        <Square />
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className = "board-row">
+        <Square />
+        <Square />
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </>
+  )
+}
