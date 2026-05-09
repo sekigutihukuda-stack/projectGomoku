afc73014976a32b0c1b58e03c1e554eb7ed4a46e
TaketoFukuda <sekiguti.hukuda@gmail.com>
2026-05-09 14:28:03 +0900

    細かな配置の調整

--- src/App.jsx
+++ src/App.jsx
@@ -133,7 +133,7 @@ function Observer({ onObserverClick }) {
   //観測ボタンの定義
   return (
     <button className="observer" onClick={onObserverClick}>
-      観測する
+      Observe!
     </button>
   );
 }
@@ -258,45 +258,46 @@ export default function Board() {
           <OrbitControls />
         </Canvas>
       </div>
-
-      <div className="board">
-        <div className="board-row">
-          <Square onSquareClick={() => handleClick(0, 0)} />
-          <Square onSquareClick={() => handleClick(0, 1)} />
-          <Square onSquareClick={() => handleClick(0, 2)} />
-          <Square onSquareClick={() => handleClick(0, 3)} />
-          <Square onSquareClick={() => handleClick(0, 4)} />
+      <div className="game-controals">
+        <div className="board">
+          <div className="board-row">
+            <Square onSquareClick={() => handleClick(0, 0)} />
+            <Square onSquareClick={() => handleClick(0, 1)} />
+            <Square onSquareClick={() => handleClick(0, 2)} />
+            <Square onSquareClick={() => handleClick(0, 3)} />
+            <Square onSquareClick={() => handleClick(0, 4)} />
+          </div>
+          <div className="board-row">
+            <Square onSquareClick={() => handleClick(1, 0)} />
+            <Square onSquareClick={() => handleClick(1, 1)} />
+            <Square onSquareClick={() => handleClick(1, 2)} />
+            <Square onSquareClick={() => handleClick(1, 3)} />
+            <Square onSquareClick={() => handleClick(1, 4)} />
+          </div>
+          <div className="board-row">
+            <Square onSquareClick={() => handleClick(2, 0)} />
+            <Square onSquareClick={() => handleClick(2, 1)} />
+            <Square onSquareClick={() => handleClick(2, 2)} />
+            <Square onSquareClick={() => handleClick(2, 3)} />
+            <Square onSquareClick={() => handleClick(2, 4)} />
+          </div>
+          <div className="board-row">
+            <Square onSquareClick={() => handleClick(3, 0)} />
+            <Square onSquareClick={() => handleClick(3, 1)} />
+            <Square onSquareClick={() => handleClick(3, 2)} />
+            <Square onSquareClick={() => handleClick(3, 3)} />
+            <Square onSquareClick={() => handleClick(3, 4)} />
+          </div>
+          <div className="board-row">
+            <Square onSquareClick={() => handleClick(4, 0)} />
+            <Square onSquareClick={() => handleClick(4, 1)} />
+            <Square onSquareClick={() => handleClick(4, 2)} />
+            <Square onSquareClick={() => handleClick(4, 3)} />
+            <Square onSquareClick={() => handleClick(4, 4)} />
+          </div>
         </div>
-        <div className="board-row">
-          <Square onSquareClick={() => handleClick(1, 0)} />
-          <Square onSquareClick={() => handleClick(1, 1)} />
-          <Square onSquareClick={() => handleClick(1, 2)} />
-          <Square onSquareClick={() => handleClick(1, 3)} />
-          <Square onSquareClick={() => handleClick(1, 4)} />
-        </div>
-        <div className="board-row">
-          <Square onSquareClick={() => handleClick(2, 0)} />
-          <Square onSquareClick={() => handleClick(2, 1)} />
-          <Square onSquareClick={() => handleClick(2, 2)} />
-          <Square onSquareClick={() => handleClick(2, 3)} />
-          <Square onSquareClick={() => handleClick(2, 4)} />
-        </div>
-        <div className="board-row">
-          <Square onSquareClick={() => handleClick(3, 0)} />
-          <Square onSquareClick={() => handleClick(3, 1)} />
-          <Square onSquareClick={() => handleClick(3, 2)} />
-          <Square onSquareClick={() => handleClick(3, 3)} />
-          <Square onSquareClick={() => handleClick(3, 4)} />
-        </div>
-        <div className="board-row">
-          <Square onSquareClick={() => handleClick(4, 0)} />
-          <Square onSquareClick={() => handleClick(4, 1)} />
-          <Square onSquareClick={() => handleClick(4, 2)} />
-          <Square onSquareClick={() => handleClick(4, 3)} />
-          <Square onSquareClick={() => handleClick(4, 4)} />
-        </div>
+        <Observer onObserverClick={() => calculateWinner(squares)} />
       </div>
-      <Observer onObserverClick={() => calculateWinner(squares)} />
     </>
   );
 }
